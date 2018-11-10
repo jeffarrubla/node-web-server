4002d29f56e577562ef8d2651fb7f8a761c85210
Jeff <none>
2018-11-10 19:55:02 +0000

    Correct error on port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs'); //http://handlebarsjs.com/
 const fs = require('fs');
 
-const port = process.env.POST || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
